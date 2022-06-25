5ca7d1b8ee9ebba48f950c8f236cd7d2e3ba9d38
Herman Ciechanowiec <herman@ciechanowiec.eu>
2022-06-25 23:50:10 +0200

    Add example 8

---
+++ src/main/java/tutorial/example8/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * The example 8 was supposed to serve as a demonstration of Service
+ * Binder (org.apache.felix.servicebinder) functionalities. However,
+ * the Service Binder has been removed from the Apache Felix, so
+ * it wasn't implemented here
+ */
+package tutorial.example8;
